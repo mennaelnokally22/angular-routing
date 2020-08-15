37aca5a5fb0f2fef31d5b29b7eb8e61e8957bbb4
Menna <menna.elnokally@gmail.com>
2020-08-15 14:12:38 +0200

    🎨 IMPROVE: Types added for http service methods

--- src/app/_services/blogs.service.ts
+++ src/app/_services/blogs.service.ts
@@ -2,19 +2,22 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { environment } from "../../environments/environment";
 import { Observable } from "rxjs";
+import { Blog } from "../_models/blog";
 @Injectable({
   providedIn: "root",
 })
 export class BlogsService {
   constructor(private httpService: HttpClient) {}
 
-  getPagedBlogs(pageNum: Number): Observable<any> {
-    return this.httpService.get(
+  getPagedBlogs(pageNum: Number): Observable<{ blogs: Blog[]; count: number }> {
+    return this.httpService.get<{ blogs: Blog[]; count: number }>(
       `${environment.backendBaseUrl}/blog/pages/${pageNum}`
     );
   }
 
-  getBlogById(blogId: string): Observable<any> {
-    return this.httpService.get(`${environment.backendBaseUrl}/blog/${blogId}`);
+  getBlogById(blogId: string): Observable<Blog> {
+    return this.httpService.get<Blog>(
+      `${environment.backendBaseUrl}/blog/${blogId}`
+    );
   }
 }
