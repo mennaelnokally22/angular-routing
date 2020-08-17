141988afbce97978fa5a4fb152416af6930c58f2
Menna <menna.elnokally@gmail.com>
2020-08-17 22:53:34 +0200

    ✨ Token added to the request

--- src/app/_services/interceptor.service.ts
+++ src/app/_services/interceptor.service.ts
@@ -6,16 +6,27 @@ import {
   HttpEvent,
 } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { AuthService } from "./auth.service";
 
 @Injectable({
   providedIn: "root",
 })
 export class InterceptorService implements HttpInterceptor {
-  constructor() {}
+  constructor(private authService: AuthService) {}
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    throw new Error("Method not implemented.");
+    console.log("INTERCEPTOR");
+
+    const token = this.authService.token;
+    console.log(token);
+    let newHeaders = req.headers;
+    if (token) {
+      newHeaders = newHeaders.append("authtoken", token);
+    }
+    const modifiedReq = req.clone({ headers: newHeaders });
+
+    return next.handle(modifiedReq);
   }
 }
