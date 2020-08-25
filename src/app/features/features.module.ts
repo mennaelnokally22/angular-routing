fe4ea5b84711a7cc0794fc5eb5f6e4942872eb8f
Menna <menna.elnokally@gmail.com>
2020-08-25 17:18:06 +0200

    ⚡ Route added for followers blogs

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -1,15 +1,21 @@
+import { MatCardModule } from "@angular/material/card";
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
 import { BlogDetailsComponent } from "./blog-details/blog-details.component";
 import { BlogResolverService } from "../_services/blog-resolver.service";
 import { FollowersBlogsComponent } from "./followers-blogs/followers-blogs.component";
-import { ProfileComponent } from './profile/profile.component';
+import { ProfileComponent } from "./profile/profile.component";
 
 @NgModule({
-  declarations: [BlogDetailsComponent, FollowersBlogsComponent, ProfileComponent],
+  declarations: [
+    BlogDetailsComponent,
+    FollowersBlogsComponent,
+    ProfileComponent,
+  ],
   imports: [
     CommonModule,
+    MatCardModule,
     RouterModule.forChild([
       {
         path: "followers-blogs",
